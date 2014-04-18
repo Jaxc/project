27585bcce5d0f59771a54a3c4ddcdcfb2f1b2221
Jaxc <coolJaxc@gmail.com>
2014-04-18 21:40:58 +0200

    random file

--- Musicplayer/pa.fromHdl.tcl
+++ Musicplayer/pa.fromHdl.tcl
@@ -1,11 +1,20 @@
 
 # PlanAhead Launch Script for Pre-Synthesis Floorplanning, created by Project Navigator
 
-create_project -name musicplayer -dir "D:/Dropbox/Dropbox/github/musicplayer/project/Musicplayer/planAhead_run_1" -part xc3s200avq100-4
+create_project -name Musicplayer -dir "C:/Dropbox/github/musicplayer/project/Musicplayer/planAhead_run_3" -part xc6slx9ftg256-3
 set_param project.pinAheadLayout yes
 set srcset [get_property srcset [current_run -impl]]
 set_property target_constrs_file "decoder.ucf" [current_fileset -constrset]
+set hdlfile [add_files [list {ipcore_dir/DMC.vhd}]]
+set_property file_type VHDL $hdlfile
+set_property library work $hdlfile
+set hdlfile [add_files [list {ipcore_dir/DMC2.vhd}]]
+set_property file_type VHDL $hdlfile
+set_property library work $hdlfile
 set hdlfile [add_files [list {functions.vhd}]]
+set_property file_type VHDL $hdlfile
+set_property library work $hdlfile
+set hdlfile [add_files [list {dataconverter.vhd}]]
 set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
 set hdlfile [add_files [list {SDRAMInterface.vhd}]]
@@ -14,7 +23,13 @@ set_property library work $hdlfile
 set hdlfile [add_files [list {SampleENA.vhd}]]
 set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
+set hdlfile [add_files [list {modules.vhd}]]
+set_property file_type VHDL $hdlfile
+set_property library work $hdlfile
 set hdlfile [add_files [list {Decoder2.vhd}]]
+set_property file_type VHDL $hdlfile
+set_property library work $hdlfile
+set hdlfile [add_files [list {DAC_test_top.vhd}]]
 set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
 set hdlfile [add_files [list {controlunit.vhd}]]
@@ -25,4 +40,4 @@ set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
 set_property top top $srcset
 add_files [list {decoder.ucf}] -fileset [get_property constrset [current_run]]
-open_rtl_design -part xc3s200avq100-4
+open_rtl_design -part xc6slx9ftg256-3
